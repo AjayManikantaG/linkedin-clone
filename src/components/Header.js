03595bd3dec864a43e77910d0f3b1262836e7901
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-10-30 15:55:33 +0530

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+  return (
+    <div>
+      <SearchIcon />
+    </div>
+  );
+}
+
+export default Header;
