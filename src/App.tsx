312521170e5fb680556b37981e91b693c2e6f620
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-11-09 20:58:37 +0530

    initial typescript app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is ajay</h1>
+      <Counter message="This is my first title"/>
+    </div>
+  );
+}
+
+export default App;
