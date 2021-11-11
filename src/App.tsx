77885264e42ad2efd038e5314edd49a43e54eb23
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-11-12 00:01:54 +0530

    add initial main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MainSection from './components/MainSection';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header badges={true} />
+      <MainSection />
     </div>
   );
 }
---
+++ src/components/MainSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MainSection.css';
+
+function MainSection() {
+  return (
+    <main className='main__section'>
+      <div className='main__container'>
+        <p>Main section testing</p>
+      </div>
+    </main>
+  );
+}
+
+export default MainSection;
